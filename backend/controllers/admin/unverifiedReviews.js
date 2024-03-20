73ac69fb2974c93391d91c2f0cbb0e2cc6eab51b
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-03-20 22:08:20 +0530

    adds admin

---
+++ backend/controllers/admin/unverifiedReviews.js
@@ -0,0 +1,29 @@
+import pool from "../../database/db.js";
+
+const unverifiedReviews = async (req, res) => {
+  try {
+    // adminRouter.get("/unverifiedReviews/:page", allUnverifiedReviews);
+
+    const page = req.params.page;
+    const limit = 2;
+    const startIndex = (page - 1) * limit;
+
+    const reviews = await pool.query(
+      "SELECT * FROM reviews WHERE isadminverified = false ORDER BY review_id DESC LIMIT $1 OFFSET $2",
+      [limit, startIndex]
+    );
+
+    return res.status(200).json({
+      success: true,
+      reviews: reviews.rows,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export default unverifiedReviews;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,22 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import authRouter from './routes/auth.js';
+import authRouter from "./routes/auth.js";
+import adminRouter from "./routes/admin.js";
 
 dotenv.config();
 const app = express();
 
-
 app.use(express.json());
-const port=process.env.PORT;
+const port = process.env.PORT;
 
-app.get("/",async (req,res)=>{
-    res.status(200).send({
-        success : true,
-        message : "server healthy"
-    })
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "server healthy",
+  });
 });
 
-app.use("/auth",authRouter);
-
-
+app.use("/auth", authRouter);
+app.use("/admin", adminRouter);
 
 app.listen(port);
