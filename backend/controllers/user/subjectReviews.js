5288a3701644c8ca55b45eadc569529326f3022a
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-20 21:56:03 +0530

    completed get routes

---
+++ backend/controllers/user/subjectReviews.js
@@ -0,0 +1,35 @@
+import pool from "../../database/db.js";
+import _ from "lodash";
+
+const subjectReviewsController = async (req, res) => {
+  try {
+    const { courseCode, departmentName } = req.params;
+    console.log(req.params);
+    const dept_name = _.startCase(_.replace(departmentName, /-/g, ' '));
+    console.log(dept_name);
+
+    const subjectReviewsQuery = `
+      SELECT r.details, r.stars, r.review_date 
+      FROM reviews AS r 
+      JOIN users AS u ON r.user_id = u.id 
+      JOIN subjects AS s ON r.subject_id = s.subject_id 
+      JOIN departments AS d ON s.department_id = d.department_id 
+      WHERE d.department_name = $1 AND s.course_code = $2 AND r.isadminverified = $3;
+    `;
+
+    const subjectReviews = await pool.query(subjectReviewsQuery, [dept_name, courseCode, true]);
+    return res.json({
+      success: true,
+      message: 'Fetched reviews successfully',
+      subjectReviews: subjectReviews.rows // Assuming the result is an array of rows
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+    });
+  }
+};
+
+export default subjectReviewsController;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,8 @@ import usernameController from "../controllers/user/username.js";
 import allDepartmentsController from "../controllers/user/allDepartments.js";
 import allSubjectsController from "../controllers/user/allSubjects.js";
 import pagedSubjectsController from "../controllers/user/pagedSubjects.js";
+import departmentSubjectsController from "../controllers/user/departmentSubjects.js";
+import subjectReviewsController from "../controllers/user/subjectReviews.js";
 
 const userRouter = express.Router();
 
@@ -11,5 +13,7 @@ userRouter.get("/username", verifyMiddleware,usernameController);
 userRouter.get("/allDepartments",verifyMiddleware,allDepartmentsController);
 userRouter.get("/allSubjects",verifyMiddleware,allSubjectsController);
 userRouter.get("/subjects/:page", verifyMiddleware,pagedSubjectsController);
+userRouter.get("/allDepartments/:departmentName",verifyMiddleware,departmentSubjectsController);
+userRouter.get("/allDepartments/:departmentName/:courseCode",verifyMiddleware,subjectReviewsController)
 
 export default userRouter;
