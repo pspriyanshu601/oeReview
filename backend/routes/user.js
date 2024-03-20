51b658913459b8a9ea69c24e7ee6d73e38919ed3
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-03-21 01:16:26 +0530

    working?

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,50 +6,10 @@ import allSubjectsController from "../controllers/user/allSubjects.js";
 import pagedSubjectsController from "../controllers/user/pagedSubjects.js";
 import departmentSubjectsController from "../controllers/user/departmentSubjects.js";
 import subjectReviewsController from "../controllers/user/subjectReviews.js";
-import jwt from "jsonwebtoken";
 
 const userRouter = express.Router();
 
-userRouter.get("/", async (req, res) => {
-  res.status(200).send({
-    success: true,
-    message: "user is healthy no worry",
-  });
-});
-
-userRouter.post("/", async (req, res) => {
-  res.status(200).send({
-    success: true,
-    message: "user is healthy no worry",
-  });
-});
-
-userRouter.get(
-  "/username",
-  async (req, res, next) => {
-    try {
-      const token = req.headers.authorization;
-      try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = decoded.id;
-      } catch (error) {
-        return res.status(400).json({
-          success: false,
-          message: "Unauthorized User",
-        });
-      }
-      next();
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: "internal server error",
-      });
-    }
-  },
-  usernameController
-);
-
-// userRouter.get("/username", verifyMiddleware, usernameController);
+userRouter.get("/username", verifyMiddleware, usernameController);
 userRouter.get("/allDepartments", verifyMiddleware, allDepartmentsController);
 userRouter.get("/allSubjects", verifyMiddleware, allSubjectsController);
 userRouter.get("/subjects/:page", verifyMiddleware, pagedSubjectsController);
