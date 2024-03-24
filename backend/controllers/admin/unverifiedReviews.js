dd986e5e45f9cfcd56ba7e97bb4748f1f2ac65b2
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-03-24 12:25:45 +0530

    page=0 for admin

--- backend/controllers/admin/unverifiedReviews.js
+++ backend/controllers/admin/unverifiedReviews.js
@@ -5,8 +5,21 @@ const unverifiedReviews = async (req, res) => {
     // adminRouter.get("/unverifiedReviews/:page", allUnverifiedReviews);
 
     const page = req.params.page;
+
     const limit = 10;
     const startIndex = (page - 1) * limit;
+
+    // if page == 0 return all
+    if (page == 0) {
+      const reviews = await pool.query(
+        "SELECT * FROM reviews WHERE isadminverified = false ORDER BY review_id DESC"
+      );
+
+      return res.status(200).json({
+        success: true,
+        reviews: reviews.rows,
+      });
+    }
 
     const reviews = await pool.query(
       "SELECT * FROM reviews WHERE isadminverified = false ORDER BY review_id DESC LIMIT $1 OFFSET $2",
