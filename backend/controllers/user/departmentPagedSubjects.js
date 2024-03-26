c1053055e7cdc776bc8f5c6bc8dec2b2d297d2b6
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-26 17:52:07 +0530

    bug fix

--- backend/controllers/user/departmentPagedSubjects.js
+++ backend/controllers/user/departmentPagedSubjects.js
@@ -20,15 +20,17 @@ const departmentPagedSubjectsController = async (req, res) => {
       WITH WeightedSubjects AS (
         SELECT 
             s.*,
-            CAST((5 + s.stars) AS FLOAT) / (10 + s.comments * 5) AS weighted_value,
-            ROUND(CAST(s.stars AS NUMERIC) / NULLIF(s.comments, 0), 2) AS average_rating
+            CAST((5 + s.stars) AS FLOAT) / NULLIF(10 + s.comments * 5, 0) AS weighted_value,
+            CASE 
+                WHEN s.comments = 0 THEN 0  -- Handle division by zero case
+                ELSE ROUND(CAST(s.stars AS NUMERIC) / CAST(s.comments AS NUMERIC), 2) 
+            END AS average_rating
         FROM 
             subjects s
     )
     SELECT 
         ws.subject_name,
         ws.course_code,
-        ws.subject_id,
         ws.average_rating,
         d.department_name,
         ws.comments
@@ -37,8 +39,7 @@ const departmentPagedSubjectsController = async (req, res) => {
     JOIN 
         departments d ON ws.department_id = d.department_id
     WHERE 
-        d.department_id = $1
-        AND ws.comments > 0  
+        d.department_id=$1
     ORDER BY 
         ws.weighted_value DESC;    
         `;
@@ -55,28 +56,30 @@ const departmentPagedSubjectsController = async (req, res) => {
     WITH WeightedSubjects AS (
       SELECT 
           s.*,
-          CAST((5 + s.${query}_stars) AS FLOAT) / NULLIF((10 + s.comments * 5), 0) AS weighted_value,
-          ROUND(CAST(s.${query}_stars AS NUMERIC) / NULLIF(s.comments, 0), 2) AS average_${query}_rating
+          CASE
+              WHEN 10 + s.comments * 5 = 0 THEN NULL  -- Handle division by zero case
+              ELSE CAST((5 + s.attendance_stars) AS FLOAT) / CAST((10 + s.comments * 5) AS FLOAT)
+          END AS weighted_value,
+          CASE
+              WHEN s.comments = 0 THEN 0  -- Handle division by zero case
+              ELSE ROUND(CAST(s.attendance_stars AS NUMERIC) / CAST(s.comments AS NUMERIC), 2)
+          END AS average_attendance_rating
       FROM 
           subjects s
-      WHERE 
-          s.comments > 0  -- Filter out subjects with zero comments
   )
   SELECT 
       ws.subject_name,
-      ws.subject_id,
       ws.course_code,
-      ws.average_${query}_rating,
+      ws.average_attendance_rating,
       d.department_name,
       ws.comments
   FROM 
       WeightedSubjects ws
   JOIN 
       departments d ON ws.department_id = d.department_id
-  WHERE 
-      d.department_id = $1
+  WHERE d.department_id=$1
   ORDER BY 
-      ws.weighted_value DESC;    
+      ws.weighted_value DESC;   
       `;
 
     const reviews = await pool.query(pageQuery, [req.params.departmentId]);
