827c62379e7ff584e686474f345e4c4aa590eec6
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-27 15:37:30 +0530

    minor change

--- backend/validators/updateBodyValidation.js
+++ backend/validators/updateBodyValidation.js
@@ -1,34 +1,38 @@
 import z from "zod";
 
+const registerSchema = z.object({
+  details: z.string().min(1, { message: "Please add review comment" }),
+  stars: z
+    .number()
+    .int()
+    .refine((value) => value >= 1 && value <= 5, {
+      message: "Stars must be between 1 and 5",
+    }),
+  attendance_stars: z
+    .number()
+    .int()
+    .refine((value) => value >= 1 && value <= 5, {
+      message: "Attendance stars must be between 1 and 5",
+    }),
+  grades_stars: z
+    .number()
+    .int()
+    .refine((value) => value >= 1 && value <= 5, {
+      message: "Grades stars must be between 1 and 5",
+    }),
+  quality_stars: z
+    .number()
+    .int()
+    .refine((value) => value >= 1 && value <= 5, {
+      message: "Quality stars must be between 1 and 5",
+    }),
+});
 const validateUpdateBody = (body) => {
-
-  const registerSchema = z.object({
-    details: z.string(),
-    stars: z.number().int().min(1).max(5),
-    attendance_stars: z.number().int().min(1).max(5),
-    grades_stars: z.number().int().min(1).max(5),
-    quality_stars: z.number().int().min(1).max(5),
-  }).refine((data) => {
-    if (data.stars < 1 || data.stars > 5) {
-      throw new Error('Stars must be between 1 and 5');
-    }
-    if (data.attendance_stars < 1 || data.attendance_stars > 5) {
-      throw new Error('Attendance stars must be between 1 and 5');
-    }
-    if (data.grades_stars < 1 || data.grades_stars > 5) {
-      throw new Error('Grades stars must be between 1 and 5');
-    }
-    if (data.quality_stars < 1 || data.quality_stars > 5) {
-      throw new Error('Quality stars must be between 1 and 5');
-    }
-    return true;
-  });
   try {
     registerSchema.parse(body);
     return true;
   } catch (err) {
-    return false;
+    return err.errors.map((error) => error.message); 
   }
 };
-
 export default validateUpdateBody;
