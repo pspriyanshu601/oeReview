60a85288809c1b0cf65e70a29c1408307c2b0ae5
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-03-27 21:16:17 +0530

    depts card

--- frontend/src/components/AllDepartmentCard.jsx
+++ frontend/src/components/AllDepartmentCard.jsx
@@ -1,39 +1,107 @@
-import PropTypes from 'prop-types';
-import { useSetRecoilState } from 'recoil';
-import { departmentIdAtom } from '../store';
-import { useNavigate } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+// import PropTypes from 'prop-types';
+// import { useSetRecoilState } from 'recoil';
+// import { departmentIdAtom } from '../store';
+// import { useNavigate } from 'react-router-dom';
 
-export const AllDepartmentCard = ({ department }) => {
-    const navigate = useNavigate()
-    const setDepartmentId = useSetRecoilState(departmentIdAtom)
+// export const AllDepartmentCard = ({ department }) => {
+//     const navigate = useNavigate()
+//     const setDepartmentId = useSetRecoilState(departmentIdAtom)
 
-    return (
-        <div className="w-64 p-4">
-            <div className="bg-gray-900 text-white p-6 flex flex-col justify-between h-full shadow-whiteBottom">
-                <div>
-                    <img src={department.department_image} className="w-16 h-16 mx-auto mb-8" style={{ fill: 'white' }} />
-                    <p className="text-center font-bold mb-10">{department.department_name}</p>
-                </div>
-                <div className="flex justify-center">
-                    <button
-                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                     onClick={() => {
-                        setDepartmentId(department.department_id);
-                        navigate('/home/deptSubjects')
-                    }}
-                     >
-                        Visit
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
+//     return (
+//         <div className="w-64 p-4">
+//             <div className="bg-gray-900 text-white p-6 flex flex-col justify-between h-full shadow-whiteBottom">
+//                 <div>
+//                     <img src={department.department_image} className="w-16 h-16 mx-auto mb-8" style={{ fill: 'white' }} />
+//                     <p className="text-center font-bold mb-10">{department.department_name}</p>
+//                 </div>
+//                 <div className="flex justify-center">
+//                     <button
+//                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+//                      onClick={() => {
+//                         setDepartmentId(department.department_id);
+//                         navigate('/home/deptSubjects')
+//                     }}
+//                      >
+//                         Visit
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// AllDepartmentCard.propTypes = {
+//     department: PropTypes.shape({
+//         department_id: PropTypes.number.isRequired,
+//         department_image: PropTypes.string,
+//         department_name: PropTypes.string.isRequired
+//     }).isRequired
+// };
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, CardActionArea, Rating } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { courseCodeAtom, departmentIdAtom, sortAtom } from "../store";
+import { useNavigate } from "react-router-dom";
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#282c34",
+    },
+    secondary: {
+      main: "#EEEEEE",
+    },
+  },
+});
+
+export default function HomeCard({ department, width }) {
+  const navigate = useNavigate();
+  const setDepartmentId = useSetRecoilState(departmentIdAtom);
+  console.log(department);
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Card
+        sx={{
+          bgcolor: "primary.main",
+          width: (width > 870 && "400px") || "300px",
+          height: "full",
+        }}
+        onClick={() => {
+          //   setCourseCode(review.course_code);
+          //   navigate("/home/allReviews");
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={department.department_image}
+            alt={department.department_name}
+            sx={{
+              height: 60,
+              width: 60,
+              margin: "auto",
+              marginTop: 4,
+            }}
+          />
+          <CardContent>
+            <Box
+              color="secondary.main"
+              sx={{
+                gap: "5px",
+                textAlign: "center",
+              }}
+            >
+              {department.department_name}
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </ThemeProvider>
+  );
 }
-
-AllDepartmentCard.propTypes = {
-    department: PropTypes.shape({
-        department_id: PropTypes.number.isRequired,
-        department_image: PropTypes.string,
-        department_name: PropTypes.string.isRequired
-    }).isRequired
-};
--- frontend/src/pages/AllDepartments.jsx
+++ frontend/src/pages/AllDepartments.jsx
@@ -1,21 +1,27 @@
 import Loading from "./Loading";
-import { AllDepartmentCard } from "../components/AllDepartmentCard";
+import AllDepartmentCard from "../components/AllDepartmentCard";
 import { useRecoilValue } from "recoil";
 import useAuth from "../hooks/useAuth";
-import { departmentsAtom, loadingAtom } from "../store";
+import { departmentsAtom, loadingAtom, widthAtom } from "../store";
+import { useMediaQuery } from "@mui/material";
 
 export const AllDepartments = () => {
   useAuth();
   const loading = useRecoilValue(loadingAtom);
   const allDepts = useRecoilValue(departmentsAtom);
+  const width = useRecoilValue(widthAtom);
 
   if (loading) return <Loading />;
   return (
-    <div className="min-h-screen min-w-screen pt-[68px] bg-gray-50 dark:bg-gray-800 p-6 flex flex-wrap">
-      <div className="flex flex-wrap justify-center">
+    <div className="min-h-screen pt-[68px] bg-gray-50 dark:bg-gray-400">
+      <div className="flex flex-wrap gap-4 justify-center p-6 mb-12">
         {allDepts.map((dept) => {
           return (
-            <AllDepartmentCard key={dept.department_id} department={dept} />
+            <AllDepartmentCard
+              key={dept.department_id}
+              department={dept}
+              width={width}
+            />
           );
         })}
       </div>
